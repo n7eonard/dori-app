fe2d50508c670994b385eff10f9b0ef05da6b708
Nicolas LEONARD <n7eonard@gmail.com>
2017-12-04 16:26:05 +0100

    created routes for cards, pools, programs and trainings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :cards, only: [:index, :show, :new, :create, :edit, :update]
+  resources :pools, only: [:index, :show]
+  resources :programs, only: [:new, :create]
+  resources :trainings, only: [:new, :create]
 end
