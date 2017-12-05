aeb471d7f0b53c2f9ac89b6b5454c62cc9b18397
Nicolas LEONARD <n7eonard@gmail.com>
2017-12-05 11:12:01 +0100

    changed the redirect authorized url and devise.rb setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
+
+def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+        user = User.create(name: data['name'],
+           email: data['email'],
+           password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+end
+
+
 end
