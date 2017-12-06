92edfb725e3dd5491f5aa21ac8d2f243f9865cac
Francois <rabanel.francois@gmail.com>
2017-12-06 16:04:12 +0100

    Des tentatives de seed qui echouent lamentablements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# require 'open-uri'
-# require 'nokogiri'
-
-
-# piscine = 'piscine-suzanne-berlioux-les-halles-2916'
-
-# url = 'http://equipement.paris.fr/' + piscine
-# p url
-# html_file = open(url).read
-# piscines = Nokogiri::HTML(html_file)
-
-# piscines.search('.fiche').each do |element|
-#   Pool.create(name: element.text.strip)
-# end
-
-# piscines.search('.information').each do |element|
-#   Pool.create(phone: element.search('span').text)
-# end
-
+Pool.create(name: 'PISCINE SUZANNE BERLIOUX (LES HALLES)',
+            address: 'forum des Halles 75001 PARIS',
+            opening_times: '',
+            price: '',
+            phone: '',
+            metro: '',
+            open: true,
+            size:,
+            lane:,
+            treatment: '')
