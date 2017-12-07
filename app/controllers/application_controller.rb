c826f90952d6563c2a45824e171ebc78aac090e9
Francois <rabanel.francois@gmail.com>
2017-12-07 18:40:32 +0100

    open graph fb et twitter et pages erreures 500 404 et 422

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
 end
