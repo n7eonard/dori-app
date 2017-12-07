b3d1dd5f466bde857d5ed25be346cc110a1658af
Nicolas Amiel <n.amiel.andersen@gmail.com>
2017-12-07 21:02:04 +0100

    perfect alignement of active circles

--- app/javascript/form1.js
+++ app/javascript/form1.js
@@ -37,7 +37,7 @@ const imgdebutant = document.getElementById('debutant')
 function active1 () {
   imgintermediaire.setAttribute("src", "/assets/Image intermediaire-269d8164b718e42f4cdbb2455be8aa923c025ca1c3fb8b2b144ed747865b4e0b.png");
   imgexpert.setAttribute("src", "/assets/image expert-7577e2552056366b8c77971e131e674cac07c325e89efec090f7d601e6910ba1.png");
-  imgdebutant.setAttribute("src", "/assets/Debutant selected-a105feedc5646347cd07148cdefb91af6e09942955d296a8d3c591595efc95e3.png");
+  imgdebutant.setAttribute("src", "/assets/Debutant selected-92afd351cb041be226f8a2f7bc4cc8d10a106bac59e62f4653296f92a3f2bd85.png");
   imgdebutant.classList.add("niveau-actif-debutant");
 }
 
@@ -54,7 +54,7 @@ const imgintermediaire = document.getElementById('intermediaire')
 function active2 () {
   imgexpert.setAttribute("src", "/assets/image expert-7577e2552056366b8c77971e131e674cac07c325e89efec090f7d601e6910ba1.png");
   imgdebutant.setAttribute("src", "/assets/Image debutant-714cb5fcb9a5eef8d1884c25b88f5b59a7c7a66b67e6491a5dde7417aefb8be3.png");
-  imgintermediaire.setAttribute("src", "/assets/Intermediate selected-1a2e78761a8d743556a5c1e42deff59872f1dbd0d0958b80e25cdabc99541994.png");
+  imgintermediaire.setAttribute("src", "/assets/Intermediate selected-9ffcae130ca4d1f552625f1ec5ea072599e4020c9553deb5314a91d6ba6462ca.png");
   imgintermediaire.classList.add("niveau-actif-intermediaire");
 }
 
@@ -71,7 +71,7 @@ const imgexpert = document.getElementById('expert')
 function active3 () {
   imgintermediaire.setAttribute("src", "/assets/Image intermediaire-269d8164b718e42f4cdbb2455be8aa923c025ca1c3fb8b2b144ed747865b4e0b.png");
   imgdebutant.setAttribute("src", "/assets/Image debutant-714cb5fcb9a5eef8d1884c25b88f5b59a7c7a66b67e6491a5dde7417aefb8be3.png");
-  imgexpert.setAttribute("src", "/assets/Expert selected-c38c2aa9a6e60eece23e7edf3f571ea483251d03408979ebea181c4404159af5.png");
+  imgexpert.setAttribute("src", "/assets/Expert selected-d692401a823059ee4e407a6ead8a5d9d259ca749d0dc4c7f5679c3bdce31226e.png");
   imgexpert.classList.add("niveau-actif-expert");
 }
 
