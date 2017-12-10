e3dc4ed5cbe1850af4391561fae9aa862589495b
Nicolas Amiel <n.amiel.andersen@gmail.com>
2017-12-10 14:15:20 +0100

    adding js behaviour to arrow

--- app/javascript/form2.js
+++ app/javascript/form2.js
@@ -3,3 +3,13 @@ flèche_2.addEventListener("click", (event) => {
   document.getElementById('div-heures').classList.add("hidden");
   document.getElementById('div-adresses').classList.remove("hidden");
 });
+
+const start_hours = document.querySelectorAll("textarea[id*='start']");
+const start_hours_size = start_hours.length
+var sum_of_existant = 0
+start_hours.forEach((hour) => {
+  if (hour.value != "") {
+    sum_of_existant += 1
+  }
+  console.log(sum_of_existant)
+});
