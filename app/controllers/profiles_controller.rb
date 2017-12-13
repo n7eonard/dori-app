02c4ad6ad60d5627ecdf1e7b26d090413d3edf28
Francois <rabanel.francois@gmail.com>
2017-12-13 13:01:36 +0100

    ajout de la boucle if pour afficher un bouton de creation de programme en cas d'aucun programme en base pour le current user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,6 @@ class ProfilesController < ApplicationController
     @user = current_user
     @program = Program.where(:user_id == current_user).last
     @pools = Pool.where.not(latitude: nil, longitude: nil)
-
     @markers = @pools.map do |pool|
       {
         lat: pool.latitude,
