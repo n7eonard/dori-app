6cb8b1987a2bf2271c92aa4a6130020864228d3c
Nicolas LEONARD <n7eonard@gmail.com>
2017-12-14 10:28:19 +0100

    added sweetalert.js libraitry

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,13 @@
+import swal from 'sweetalert';
+
+function bindSweetAlertButtonDemo() {
+  document.getElementById('sweet-alert-demo').addEventListener('click', () => {
+    swal({
+      title: "A nice alert",
+      text: "This is a great alert, isn't it?",
+      icon: "success"
+    })
+  });
+}
+
+export { bindSweetAlertButtonDemo };
